148b867d567751e732885c6d7eaf94a443d96771
prestonwfoote <prestonwfoote@gmail.com>
2022-02-27 12:04:18 -0500

    static prompt menu appears in terminal

--- server.js
+++ server.js
@@ -1,21 +1,76 @@
 require('dotenv').config()
-const mysql = require('mysql2')
-const app = require('./lib/app')
+const db = require('./db/connection')
+const inquirer = require('inquirer')
+const Department = require('./lib/department')
 
-// Connect to database
-const db = mysql.createConnection(
-  {
-    // dotenv protected values
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+
+// MENU PROMPT START
+const menuPrompt = [
+    {
+      type: 'list',
+      name: 'menu',
+      message: 'Select an option',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update employee role'
+      ]
+    }
+  ]
+  // MENU PROMPT END
+  
+  const app = () => {
+    mainMenu()
   }
-)
-
-db.connect(err => {
-  if (err) throw err
-  console.log('Database connected.')
+  
+  const mainMenu = () => {
+    inquirer
+      .prompt(menuPrompt)
+      .then(({ menu }) => {
+        
+        switch (menu) {
+          case 'View all departments':
+            // call SQL query for all departments
+            console.log('View all departments chosen.')
+            Department.viewAllDepts()
+            break
+          case 'View all roles':
+            // call SQL query for all roles
+            console.log('View all roles chosen.')
+            break
+          case 'View all employees':
+            // call SQL query for all employees
+            console.log('View all employees chosen.')
+            break
+          case 'Add a department':
+            // function prompt user for department name
+            // SQL query to add department 
+            console.log('Add department chosen.')
+            Department.addDept()
+            break
+          case 'Add a role':
+            // function prompt user for name, salary, and department for role
+            // SQL query to add role to database
+            console.log('Add role chosen.')
+            break
+          case 'Add an employee':
+            // function prompt user for first name, last name, role, and manager of employee
+            // SQL query to add employee to database
+            console.log('Add employee chosen.')
+            break
+          case 'Update an employee role':
+            //  prompt list for employee
+            // prompt user to enter new role for selected employee
+            // call SQL query to update employee role in database
+            console.log('Update employee role chosen.')
+        }
+      })
+  }
   
   app()
-})
+  
+  module.exports.mainMenu = mainMenu
